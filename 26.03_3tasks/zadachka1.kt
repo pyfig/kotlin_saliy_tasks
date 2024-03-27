da4d6401a2de3d55bf2d73a72835b8e076cbecdc
xdshkaaa <insober122@mail.ru>
2024-03-27 20:37:17 +0700

    formatted

--- 26.03_3tasks/zadachka1.kt
+++ 26.03_3tasks/zadachka1.kt
@@ -1,7 +1,7 @@
 fun main() {
     val morningNotification = 51
     val eveningNotification = 135
-    printNotificationSummary(morningNotification)
+    printNotificationSummary(morningNotification)   
     printNotificationSummary(eveningNotification)
     }
     fun printNotificationSummary(numberOfMessages: Int) {
